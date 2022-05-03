fead1aa0f6ba983b6a12e673e72029f19fef0b78
ChristopherBanz <christopher.banz@gmail.com>
2022-05-03 17:21:00 -0400

    okay now its a thing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
+import MadGenius from './MadGenius.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Broohaus!
-        </p>
-      </header>
+      <MadGenius/>
     </div>
   );
 }
---
+++ src/MadGenius.js
@@ -0,0 +1,15 @@
+import React from "react";
+import god from "./img/MadGenius.png";
+
+
+
+
+export default function MadGeius(){
+    
+    return(
+        <div>
+            <img src = {god} alt="obey my dog"></img>
+            <p> BROOHAUS!!!</p>
+        </div>
+    )
+}
